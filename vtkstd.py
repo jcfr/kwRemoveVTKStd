f3e3cfdb4cae4667ccfb6b250906fe3058bdc091
Robert Maynard <robert.maynard@kitware.com>
2012-01-04 12:03:38 -0500

    Cleaned up the script to be easier to use.
    Added in arguments to specify the root directory, if it should recurse,
    directory names it should ignore and the verbosity level.

--- vtkstd.py
+++ vtkstd.py
@@ -1,59 +1,74 @@
-#!/usr/bin/env python2
-import fileinput, glob, string, sys, os, re
+#!/usr/bin/env python
+import fileinput, glob, string, sys, os, re, argparse
 
 # Build up a list of all export macros to replace.
 pattern = re.compile("#include <vtkstd/")
 patterna = re.compile("#include \"vtkstd/")
 pattern2 = re.compile("vtkstd::")
 
-def searchReplace(path):
-  files = glob.glob(path)
-  if files is not []:
-    for file in files:
-      if os.path.isfile(file):
-        for line in fileinput.input(file, inplace=1):
-          lineNumber = pattern.search(line)
-          if lineNumber >= 0:
-            #print "Found a line to replace: " + line
-            line = pattern.sub("#include <", line)
-            line = line.rstrip() + "\n"
-          lineNumber = patterna.search(line)
-          if lineNumber >= 0:
-            line = patterna.sub("#include \"", line)
-            line = line.rstrip() + "\n"
-          lineNumber = pattern2.search(line)
-          if lineNumber >= 0:
-            line = pattern2.sub("std::", line)
-            line = line.rstrip() + "\n"
-          
-          
-          sys.stdout.write(line)
+def dirwalk(dirPath,recurse,ignore):
+  "walk a directory tree, using a generator"
+  for f in os.listdir(dirPath):
+    fullpath = os.path.join(dirPath,f)
+    if(recurse and\
+       os.path.isdir(fullpath) and\
+       not os.path.islink(fullpath) and\
+       not f.startswith('.') and\
+       not f in ignore):
+      for x in dirwalk(fullpath,recurse,ignore):
+        yield x
+  yield dirPath
 
-#searchReplace("*/*.txx")
-#searchReplace("*/*.cxx")
-#searchReplace("*/*.h*")
-#searchReplace("*/*.py")
-#searchReplace("*/Testing/Cxx/*.cxx")
-#searchReplace("*/Testing/Cxx/*.h")
-#searchReplace("Utilities/*/*.cxx")
-#searchReplace("Utilities/*/*.h*")
-#searchReplace("Wrapping/*/*.cxx")
-#searchReplace("Examples/*/Cxx/*.cxx")
-#searchReplace("Examples/*/Cxx/*.h")
-#searchReplace("GUISupport/Qt/*.h")
-#searchReplace("GUISupport/Qt/*.cxx")
+#clean all the files in a directory
+def cleanDirectory(dirPath,verbose):
+  if(verbose):
+    print "cleaning directory: ", dirPath
 
-searchReplace("Applications/ParaView/*.cxx")
-searchReplace("Applications/ParaView/*.h*")
-searchReplace("CMake/*")
-searchReplace("CommandLineExecutables/*")
-searchReplace("CoProcessing/*/*")
-searchReplace("Common/*/*")
-searchReplace("ParaView*/*/*")
-searchReplace("ParaView*/*/*/*")
-searchReplace("ParaView*/*/*/*/*")
-searchReplace("CoProcessing/CoProcessor/Testing/TestDriver/*")
-searchReplace("Examples/Plugins/*/*")
-searchReplace("Plugins/*/*")
-searchReplace("Plugins/*/*/*")
-searchReplace("Plugins/*/*/*/*htop")
+  for f in os.listdir(dirPath):
+    fullpath = os.path.join(dirPath,f)
+    if(os.path.isfile(fullpath) and\
+       not os.path.islink(fullpath) and\
+       not f.startswith('.')):
+      if(verbose):
+        print "cleaning: ", f
+      replaceFile(fullpath)
+
+def replaceFile(fname):
+  for line in fileinput.input(fname, inplace=1):
+    lineNumber = pattern.search(line)
+    if lineNumber >= 0:
+      line = pattern.sub("#include <", line)
+      line = line.rstrip() + "\n"
+    lineNumber = patterna.search(line)
+    if lineNumber >= 0:
+      line = patterna.sub("#include \"", line)
+      line = line.rstrip() + "\n"
+    lineNumber = pattern2.search(line)
+    if lineNumber >= 0:
+      line = pattern2.sub("std::", line)
+      line = line.rstrip() + "\n"
+    sys.stdout.write(line)
+
+
+def main():
+  parser = argparse.ArgumentParser(description='Remove vtkstd usage from a repository.')
+  parser.add_argument('-d', '--directory',
+                     required=True,
+                     help='root directory to parse')
+  parser.add_argument('-i', '--ignore',
+                      nargs='+',
+                      help='directory to ignore when recursing')
+
+  parser.add_argument('-R', '--recurse',
+                      action='store_true',
+                      help='recurse the root directory')
+  parser.add_argument('-v', '--verbose',
+                      action='store_true',
+                      help='verbose output')
+
+  args = parser.parse_args()
+  for dirPath in dirwalk(args.directory,args.recurse,args.ignore):
+    cleanDirectory(dirPath,args.verbose)
+
+if __name__ == '__main__':
+  main()
